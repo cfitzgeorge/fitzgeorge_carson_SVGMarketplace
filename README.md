6961a13eb7947e6bc5fe7d9d1bb8891164964778
Carson <irealgoodcf@gmail.com>
2022-01-29 23:02:30 -0500

    last changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# fitzgeorge_carson_SVGMarketplace
+# My SVG Marketplace
+Come learn about Logic Gates! 
+
+1. Installation
+Download it to your PC!
+
+2. Contributing
+- Fork it!
+- Commit your changes
+- Push your changes to the main branch
+
+Made with Visual Studio Code
--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
 (()=> {
-    let SVGIcons = document.querySelectorAll(".container img");
+let SVGIcons = document.querySelectorAll(".container img")
 
-    let SVGIcons = 1;
-    $("container img").mouseenter(function(){
-    
-        SVGIcons = SVGIcons+1;
-        if(SVGIcons%2==0)
-        {
-            $(this).addClass("myopacity");
-        }
-        else
-        {
-            $(this).removeClass("myopacity");
-        }
-    
-        $(this).addClass("change").delay(5000).queue(function(){
-            $(this).removeClass("change").dequeue();
-        });
-        
-    });
-    
- 
+SVGIcons => SVGIcons.addEventListener("click", invert())
+
+
+function invert(){
+    document.getElementById(".container img").style.filter="invert(100%)";
+    }
+
 })();
